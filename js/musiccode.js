725ce8a4b980cd47b40639301fa921ff416312a3
Joseph Gonzalez <flipside049@gmail.com>
2018-10-06 23:37:10 -0700

    fixes for musicbox project

--- js/musiccode.js
+++ js/musiccode.js
@@ -1,36 +1,36 @@
-const cNote = document.getElementById('cAudio');
-const dNote = document.getElementById('dAudio');
-const eNote = document.getElementById('eAudio');
-const fNote = document.getElementById('fAudio');
-const gNote = document.getElementById('gAudio');
-const aNote = document.getElementById('aAudio');
-const bNote = document.getElementById('bAudio');
 
-$('#c').mousedown(function(){
-    cNote.currentTime = 0;
-    cNote.play();
-});
-$('#d').mousedown(function(){
-    dNote.currentTime = 0;
-    dNote.play();
-});
-$('#e').mousedown(function(){
-    eNote.currentTime = 0;
-    eNote.play();
-});
-$('#f').mousedown(function(){
-    fNote.currentTime = 0;
-    fNote.play();
-});
-$('#g').mousedown(function(){
-    gNote.currentTime = 0;
-    gNote.play();
-});
-$('#a').mousedown(function(){
-    aNote.currentTime = 0;
-    aNote.play();
-});
-$('#b').mousedown(function(){
-    bNote.currentTime = 0;
-    bNote.play();
-});
+  function play(){
+       var audio = document.getElementById("cAudio");
+       cAudio.currentTime = 0;
+       audio.play();
+                 }
+  function play(){
+       var audio = document.getElementById("dAudio");
+       dAudio.currentTime = 0;
+       audio.play();
+                 }
+ function play(){
+       var audio = document.getElementById("eAudio");
+       eAudio.currentTime = 0;
+       audio.play();
+                 }
+ function play(){
+       var audio = document.getElementById("fAudio");
+       fAudio.currentTime = 0;
+       audio.play();
+                 }
+  function play(){
+       var audio = document.getElementById("gAudio");
+       gAudio.currentTime = 0;
+       audio.play();
+                 }
+  function play(){
+       var audio = document.getElementById("aAudio");
+       aAudio.currentTime = 0;
+       audio.play();
+                 }
+  function play(){
+       var audio = document.getElementById("bAudio");
+       bAudio.currentTime = 0;
+       audio.play();
+                 }
